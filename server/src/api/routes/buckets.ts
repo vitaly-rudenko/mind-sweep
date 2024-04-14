e8b36c04384e291073beb4d01736405489009446
Vitaly Rudenko <maurice.dederick@gmail.com>
2024-04-15 00:23:23 +0300

    Added sync endpoint to source bucket

--- server/src/api/routes/buckets.ts
+++ server/src/api/routes/buckets.ts
@@ -5,8 +5,11 @@ import { env } from '../../env.js'
 import { formatTelegramUserName } from '../../telegram/format-telegram-user-name.js'
 import { initDataUserSchema } from '../../web-app/schemas.js'
 import { Client } from '@notionhq/client'
-import { ApiError } from '../../errors.js'
+import { ApiError, NotFoundError } from '../../errors.js'
 import { checkWebAppSignature } from '../../web-app/check-web-app-signature.js'
+import { readNotes } from '../../notes/read-notes.js'
+import { match } from '../../templates/match.js'
+import { syncNote } from '../../notes/sync-note.js'
 
 const createBucketSchema = z.discriminatedUnion('bucketType', [
   z.object({
@@ -109,6 +112,25 @@ export function createBucketsRouter() {
         code: 'UNSUPPORTED_BUCKET_TYPE',
         status: 400,
       })
+    }
+
+    res.sendStatus(201)
+  })
+
+  router.post('/buckets/:id/syncs', async (req, res) => {
+    const userId = req.user.id
+
+    const sourceBucket = await storage.getBucketById(userId, Number(req.params.id))
+    if (!sourceBucket) throw new NotFoundError()
+
+    const links = await storage.getLinksBySourceBucketId(userId, sourceBucket.id)
+    const notes = await readNotes({ userId, bucketId: sourceBucket.id })
+
+    for (const note of notes) {
+      const link = links.find(link => !link.template || match(note.content, link.template) !== undefined)
+      if (link) {
+        await syncNote({ note, link, userId })
+      }
     }
 
     res.sendStatus(201)
