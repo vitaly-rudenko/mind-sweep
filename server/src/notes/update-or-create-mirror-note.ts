9768c94ac86141e19893df0207710c6164e4fdcf
Vitaly Rudenko <maurice.dederick@gmail.com>
2024-04-21 13:14:42 +0300

    Updated updateOrCreateMirrorNote

--- server/src/notes/update-or-create-mirror-note.ts
+++ server/src/notes/update-or-create-mirror-note.ts
@@ -1,97 +1,98 @@
 import type { Message } from 'telegraf/types'
 import { type Deps, registry } from '../registry.js'
-import type { Note } from './types.js'
+import type { MirrorNote, Note } from './types.js'
 import { TelegramError } from 'telegraf'
 import { createVendorEntityHash } from '../vendor-entities/create-vendor-entity-hash.js'
 import { createTelegramVendorEntity } from '../telegram/create-telegram-vendor-entity.js'
-import { isNoteStoredInMirrorBucket } from './is-note-stored-in-mirror-bucket.js'
-import { logger } from '../logging/logger.js'
+import { deleteMirrorNote } from './delete-mirror-note.js'
+import { NotFoundError, UnsupportedActionError } from '../errors.js'
 
 export async function updateOrCreateMirrorNote(
-  input: {
-    userId: number
-    mirrorBucketId: number
-    note: Note
-  },
-  { storage, telegram }: Deps<'storage' | 'telegram'> = registry.export()
-): Promise<Note> {
+  input: { userId: number, mirrorBucketId: number, note: Note },
+  { storage }: Deps<'storage'> = registry.export()
+): Promise<MirrorNote> {
   const { userId, mirrorBucketId, note } = input
 
   const mirrorBucket = await storage.getBucketById(userId, mirrorBucketId)
-  if (!mirrorBucket) throw new Error('Mirror bucket not found')
+  if (!mirrorBucket) throw new NotFoundError('MirrorBucket not found', { mirrorBucketId })
 
-  if (note.mirrorVendorEntity && !isNoteStoredInMirrorBucket(note, mirrorBucket)) {
-    throw new Error('Note does not belong to mirror bucket')
-  }
+  // TODO: check if Note belongs to the Bucket?
 
   if (mirrorBucket.bucketType === 'telegram_chat') {
-    if (note.mirrorVendorEntity) {
-      if (note.mirrorVendorEntity.vendorEntityType === 'telegram_message') {
-        try {
-          let message: Message.TextMessage | true = await telegram.editMessageText(note.mirrorVendorEntity.metadata.chatId, note.mirrorVendorEntity.metadata.messageId, undefined, note.content)
-          if (message !== true) {
-            return {
-              ...note,
-              mirrorVendorEntity: createTelegramVendorEntity(message),
-            }
-          }
-        } catch (err) {
-          if (err instanceof TelegramError && ['Bad Request: message to edit not found', 'Bad Request: message can\'t be edited'].includes(err.response.description)) {
-            let message: Message.TextMessage
-            try {
-              message = await telegram.sendMessage(mirrorBucket.metadata.chatId, note.content, {
-                reply_parameters: {
-                  chat_id: note.mirrorVendorEntity.metadata.chatId,
-                  message_id: note.mirrorVendorEntity.metadata.messageId,
-                }
-              })
-            } catch (err) {
-              if (err instanceof TelegramError && err.response.description === 'Bad Request: message to reply not found') {
-                message = await telegram.sendMessage(mirrorBucket.metadata.chatId, note.content)
-              } else {
-                throw err
-              }
-            }
+    return telegramBucketUpdateOrCreateMirrorNote({ userId, mirrorBucketId, note })
+  } else {
+    throw new Error(`Unsupported mirror bucket type: ${mirrorBucket.bucketType}`)
+  }
+}
 
-            // Remove old message if possible
-            try {
-              await telegram.deleteMessage(note.mirrorVendorEntity.metadata.chatId, note.mirrorVendorEntity.metadata.messageId)
-            } catch (err) {
-              if (err instanceof TelegramError && err.response.description !== 'Bad Request: message to delete not found') {
-                logger.error({ err }, 'Could not delete message')
-              }
-            }
+async function telegramBucketUpdateOrCreateMirrorNote(
+  input: { userId: number; note: Note; mirrorBucketId: number },
+  { storage, telegram }: Deps<'storage' | 'telegram'> = registry.export()
+): Promise<MirrorNote> {
+  const { userId, mirrorBucketId, note } = input
 
-            return {
-              ...note,
-              mirrorVendorEntity: createTelegramVendorEntity(message),
-            }
-          } else if (err instanceof TelegramError && err.response.description === 'Bad Request: message is not modified: specified new message content and reply markup are exactly the same as a current content and reply markup of the message') {
-            // ok
-          } else {
-            throw err
-          }
-        }
+  const mirrorBucket = await storage.getBucketById(userId, mirrorBucketId)
+  if (!mirrorBucket) throw new NotFoundError('MirrorBucket not found', { mirrorBucketId })
+  if (mirrorBucket.bucketType !== 'telegram_chat') throw new UnsupportedActionError('Unsupported MirrorBucketType', { mirrorBucketType: mirrorBucket.bucketType })
 
-        return {
-          ...note,
-          mirrorVendorEntity: {
-            ...note.mirrorVendorEntity,
-            hash: createVendorEntityHash(note.content),
-          }
-        }
-      } else {
-        throw new Error('Unsupported vendor entity type for given bucket type')
-      }
-    } else {
-      const message = await telegram.sendMessage(mirrorBucket.metadata.chatId, note.content)
+  if (!note.mirrorVendorEntity) {
+    const message = await telegram.sendMessage(mirrorBucket.metadata.chatId, note.content)
 
+    return {
+      ...note,
+      mirrorVendorEntity: createTelegramVendorEntity(message),
+    }
+  }
+
+  if (note.mirrorVendorEntity.vendorEntityType !== 'telegram_message') {
+    throw new UnsupportedActionError('Unsupported MirrorVendorEntityType', { mirrorVendorEntityType: note.mirrorVendorEntity.vendorEntityType })
+  }
+
+  try {
+    let message: Message.TextMessage | true = await telegram.editMessageText(note.mirrorVendorEntity.metadata.chatId, note.mirrorVendorEntity.metadata.messageId, undefined, note.content)
+    if (message !== true) {
       return {
         ...note,
         mirrorVendorEntity: createTelegramVendorEntity(message),
       }
     }
-  } else {
-    throw new Error(`Unsupported mirror bucket type: ${mirrorBucket.bucketType}`)
+  } catch (err) {
+    if (err instanceof TelegramError && ['Bad Request: message to edit not found', 'Bad Request: message can\'t be edited'].includes(err.response.description)) {
+      let message: Message.TextMessage
+      try {
+        message = await telegram.sendMessage(mirrorBucket.metadata.chatId, note.content, {
+          reply_parameters: {
+            chat_id: note.mirrorVendorEntity.metadata.chatId,
+            message_id: note.mirrorVendorEntity.metadata.messageId,
+          }
+        })
+      } catch (err) {
+        if (err instanceof TelegramError && err.response.description === 'Bad Request: message to reply not found') {
+          message = await telegram.sendMessage(mirrorBucket.metadata.chatId, note.content)
+        } else {
+          throw err
+        }
+      }
+
+      // Try to delete the old message
+      await deleteMirrorNote({ mirrorVendorEntity: note.mirrorVendorEntity })
+
+      return {
+        ...note,
+        mirrorVendorEntity: createTelegramVendorEntity(message),
+      }
+    } else if (err instanceof TelegramError && err.response.description === 'Bad Request: message is not modified: specified new message content and reply markup are exactly the same as a current content and reply markup of the message') {
+      // ok
+    } else {
+      throw err
+    }
+  }
+
+  return {
+    ...note,
+    mirrorVendorEntity: {
+      ...note.mirrorVendorEntity,
+      hash: createVendorEntityHash(note.content),
+    }
   }
 }
