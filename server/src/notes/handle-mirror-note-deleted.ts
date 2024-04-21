d7262713e35eb08e26fa00d5d903250a23e5a8b7
Vitaly Rudenko <maurice.dederick@gmail.com>
2024-04-21 12:59:52 +0300

    Updated handleMirrorNoteDeleted()

--- server/src/notes/handle-mirror-note-deleted.ts
+++ server/src/notes/handle-mirror-note-deleted.ts
@@ -1,4 +1,5 @@
 import type { BucketQuery } from '../buckets/types.js'
+import { NotFoundError } from '../errors.js'
 import { type Deps, registry } from '../registry.js'
 import type { VendorEntityQuery } from '../vendor-entities/types.js'
 import { deleteSourceNote } from './delete-source-note.js'
@@ -13,8 +14,8 @@ export async function handleMirrorNoteDeleted(
 ): Promise<void> {
   const { userId, mirrorBucketQuery, mirrorVendorEntityQuery } = input
 
-  const mirrorBucket = await storage.getBucketByQueryId(userId, mirrorBucketQuery)
-  if (!mirrorBucket) throw new Error('Bucket not found')
+  const mirrorBucket = await storage.queryBucket(userId, mirrorBucketQuery)
+  if (!mirrorBucket) throw new NotFoundError('MirrorBucket not found', { mirrorBucketQuery })
 
   const sourceBuckets = await storage.getLinkedSourceBuckets(userId, mirrorBucket.id)
   for (const sourceBucket of sourceBuckets) {
