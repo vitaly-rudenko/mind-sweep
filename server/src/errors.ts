16e6e81b26e450b90009d9bcf85f003c418e90b5
Vitaly Rudenko <maurice.dederick@gmail.com>
2024-04-21 12:29:24 +0300

    Added UnsupportedActionError

--- server/src/errors.ts
+++ server/src/errors.ts
@@ -18,25 +18,31 @@ export class ApiError extends Error {
 }
 
 export class NotFoundError extends ApiError {
-  constructor(message = 'Resource not found') {
-    super({ code: 'NOT_FOUND', status: 404, message })
+  constructor(message = 'Resource not found', context?: unknown) {
+    super({ code: 'NOT_FOUND', status: 404, message , context})
   }
 }
 
 export class NotAuthenticatedError extends ApiError {
-  constructor(message = 'Not authenticated') {
-    super({ code: 'NOT_AUTHENTICATED', status: 401, message })
+  constructor(message = 'Not authenticated', context?: unknown) {
+    super({ code: 'NOT_AUTHENTICATED', status: 401, message , context})
   }
 }
 
 export class NotAuthorizedError extends ApiError {
-  constructor(message = 'Access denied') {
-    super({ code: 'NOT_AUTHORIZED', status: 403, message })
+  constructor(message = 'Access denied', context?: unknown) {
+    super({ code: 'NOT_AUTHORIZED', status: 403, message, context })
   }
 }
 
 export class AlreadyExistsError extends ApiError {
-  constructor(message = 'Resource already exists') {
-    super({ code: 'ALREADY_EXISTS', status: 409, message })
+  constructor(message = 'Resource already exists', context?: unknown) {
+    super({ code: 'ALREADY_EXISTS', status: 409, message , context})
+  }
+}
+
+export class UnsupportedActionError extends ApiError {
+  constructor(message = 'Unsupported action', context?: unknown) {
+    super({ code: 'UNSUPPORTED_ACTION', status: 400, message , context})
   }
 }
